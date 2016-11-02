d5b4b5ae959a11d0ebd79189eab4675bb5f55688
Ankit <ankitmaharjankath@gmail.com>
2016-11-02 11:26:48 +0545

    another error fix

---
+++ src/main/java/net/BukkitPE/resourcepacks/ResourcePackInfoEntry.java
@@ -0,0 +1,20 @@
+package net.BukkitPE.resourcepacks;
+
+public class ResourcePackInfoEntry {
+
+    protected String packId;
+    protected String version;
+    protected long uint64;
+
+    public String getPackId() {
+        return packId;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public long getUint64() {
+        return uint64;
+    }
+}
